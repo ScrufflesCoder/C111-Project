5c0eb25808321c7fc2b6a45eaf452a54ee4b0338
ScrufflesCoder <138102755+ScrufflesCoder@users.noreply.github.com>
2024-06-09 17:03:55 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//https://teachablemachine.withgoogle.com/models/fxdFFfXbP/
+
+    Webcam.set({
+        width:350,
+        height:300,
+        image_format:'jpg',
+        jpg_quality:90
+    });
+
+    camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+        Webcam.snap(function(data_uri) {
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fxdFFfXbP/model.json');
+
